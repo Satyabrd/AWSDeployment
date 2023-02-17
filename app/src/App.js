cf1e1e2d46b74ca83f577305e0d0fcc60d0cd57b
Satyabrata Dash <satyabrd135@gmail.com>
2023-02-17 10:39:32 +0530

    feat: Just pushing a test button and text

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button>Click me</button>
+        <p>This is a test project</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
